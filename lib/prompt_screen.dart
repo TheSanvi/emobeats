4784d3298821efc8fb033cb7287a672e80c78693
TheSanvi <sanvivermavns@gmail.com>
2024-10-01 23:08:00 +0530

    Update prompt_screen.dart

--- lib/prompt_screen.dart
+++ lib/prompt_screen.dart
@@ -105,28 +105,110 @@ class _PromptScreenState extends State<PromptScreen> {
                         children: genres.map((genre) {
                           final isSelected =
                           _selectedGenres.contains(genre);
-                          return Container(
-                            padding: const EdgeInsets.all(3.0),
-                            margin: EdgeInsets.only(right: 4.0,
-                                top: 4.0),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20.0),
-                                border: Border.all(
-                                    width: 0.4,
-                                    color: const Color(0xFFFFFFFF).withOpacity(
-                                        0.8)
-                                )
+                          
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                if (_selectedGenres
+                                    .contains(genre)) {
+                                  _selectedGenres.remove(genre);
+                                } else {
+                                  _selectedGenres.add(genre);
+                                }
+                              });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(3.0),
+                              margin: EdgeInsets.only(right: 4.0,
+                                  top: 4.0),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  border: Border.all(
+                                      width: 0.4,
+                                      color: const Color(0xFFFFFFFF).withOpacity(
+                                          0.8)
+                                  )
+                              ),
+                            child: Container(
+                            padding: const EdgeInsets.only(
+                            left: 16.0,
+                            right: 16.0,
+                            top: 8.0,
+                            bottom: 8.0,
                             ),
-
-
+                            decoration: BoxDecoration(
+                            color: isSelected
+                            ? const Color(0xFF0000FF)
+                                : const Color(0xFFFFFFFF)
+                                .withOpacity(0.8),
+                            borderRadius:
+                            BorderRadius.circular(
+                            20.0),
+                            ),
+                              // Text for each genre
+                              child: Text(
+                                genre,
+                                style: GoogleFonts.inter(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w600,
+                                  color: isSelected
+                                      ? const Color(
+                                      0xFFFFFFFF)
+                                      : const Color(
+                                      0xFF000000),
+                                ),
+                              ),
+                            ),
+                            
+                            
+                            
+                            ),
                           );
+
                         }).toList(),
 
                       );
                     }
-                    ),)
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 60.0,
+                      left: 10.0,
+                      right: 10.0,
+                    ),
+    // Container for submit button in GestureDetector
+    // child: GestureDetector(
+    // onTap: _submitSelections,
 
-  ]
+    // Container for submit button
+    child: Container(
+    padding: const EdgeInsets.symmetric(
+    vertical: 15.0),
+    decoration: BoxDecoration(
+    borderRadius:
+    BorderRadius.circular(20.0),
+    color: const Color(0xFFFFCCCC),
+    ),
+      child: Center(
+        // Submit text here
+        child: Text(
+          'Submit',
+          style: GoogleFonts.inter(
+            fontSize: 14.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    ),
+                  ),
+
+
+
+
+
+
+  ],
                   ) ,
                 ),
               ),
